ca1abc1766d77d1168281ad5eee87f243281103d
Serge-Moskalenko <skmos2019@outlook.com>
2024-08-07 17:17:13 +0100

    update

--- task.js
+++ task.js
@@ -1,14 +1,14 @@
 const data=require('./addressbook.json');
-
-Object.entries(data.contacts).forEach(function([key, value]) {
+const {contacts}=data
+Object.entries(contacts).forEach(function([key, value]) {
     console.log(key, value);
 })
 
-Object.keys(data.contacts).forEach(function(key) {
+Object.keys(contacts).forEach(function(key) {
     console.log(key, data.contacts[key]);
 });
 
-for (let key in data.contacts) {
+for (let key in contacts) {
     if (data.contacts.hasOwnProperty(key)) {
         console.log(key, data.contacts[key]);
     }
